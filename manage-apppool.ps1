9dd2c585422ac70f2e358ef9b3228fae5027a80a
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-08-06 04:41:09 +0300

    Updated Workflow to stop and start app pool

---
+++ manage-apppool.ps1
@@ -0,0 +1,28 @@
+param(
+    [string]$action,
+    [string]$siteName = "site33211"
+)
+
+$server = "site33211.siteasp.net"
+$username = "site33211"
+$password = $env:FTP_PASSWORD
+
+# Upload command file via FTP
+function Send-IISCommand($command) {
+    $tempFile = [System.IO.Path]::GetTempFileName()
+    Set-Content $tempFile $command
+    curl -T $tempFile ftp://$server/apppool-command.txt -u "$username`:$password" --ftp-create-dirs
+    Remove-Item $tempFile
+}
+
+# Action logic
+switch ($action) {
+    "stop" { 
+        Send-IISCommand "APPCMD stop apppool /apppool.name:`"$siteName`""
+        Start-Sleep -Seconds 5
+    }
+    "start" { 
+        Send-IISCommand "APPCMD start apppool /apppool.name:`"$siteName`"" 
+        Start-Sleep -Seconds 5
+    }
+}
